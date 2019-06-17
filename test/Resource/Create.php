f7f37f0e94b51e9ef3629199e9b5c54a40e18b60
Hafiz Saif ul Usman <51157913+saifulusman@users.noreply.github.com>
2019-06-17 13:13:18 +0500

    Add files via upload

--- test/Resource/Create.php
+++ test/Resource/Create.php
@@ -2,5 +2,6 @@
 $payload = array('name'=>'Truck 01111','type'=>'Transport Vehicle');
 require_once('../../src/ArrivyClient.php');
 $client = new ArrivyClient('4e69ee06-783b','Kv1IwIsNHtJZeggaAqcX0C','SANDBOX');
-print_r($client->resource->Create($payload));
+$result=$client->resource->Create($payload);
+print_r($result);
 ?>
--- test/Resource/GetById.php
+++ test/Resource/GetById.php
@@ -1,5 +1,6 @@
 <?php
 require_once('../../src/ArrivyClient.php');
 $client = new ArrivyClient('4e69ee06-783b','Kv1IwIsNHtJZeggaAqcX0C','SANDBOX');
-print_r($client->resource->GetById("5632621182713856"));
+$result=$client->resource->GetById("5632621182713856");
+print_r($result);
 ?>
